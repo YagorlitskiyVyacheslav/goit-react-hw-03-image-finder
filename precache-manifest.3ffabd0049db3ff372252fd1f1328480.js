6fd29d3942282760985421c54851d526a0bc0ded
Vyacheslav <slavasan05@gmail.com>
2020-10-11 23:43:11 +0200

    Updates

---
+++ precache-manifest.3ffabd0049db3ff372252fd1f1328480.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b165bd9aee022d5ab86f51a2389a90",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "a490efc75a6b9306dae9",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.fdace960.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.fdace960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7ee007a22fc1ecfa71",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.44766661.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
